20c43ca8c729eb50a2073be5f6bf92e1309088f7
SindhujaReddy-VT <bsindhuja@vt.edu>
2023-10-05 15:56:29 -0400

    Backend changes

---
+++ back-end/userDetails.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const UserDetailSchema = new mongoose.Schema({
+    fname:String,
+    lname:String,
+    email:{type:String, unique:true},
+    password:String
+},
+{
+    collection:'UserInfo',
+})
+
+mongoose.model("UserInfo",UserDetailSchema)
