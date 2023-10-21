ddefb8db0cd2904fa8c112a9c07d453a9ef102d2
SindhujaReddy-VT <122953118+SindhujaReddy-VT@users.noreply.github.com>
2023-10-21 18:35:56 -0400

    Delete back-end directory

--- back-end/diagnoSym/diagnoSym/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class User(models.Model):
-    first_name = models.CharField(max_length=60)
-    last_name = models.CharField(max_length=60)
-    email = models.EmailField(max_length=60)
-    password = models.CharField(max_length=60)
